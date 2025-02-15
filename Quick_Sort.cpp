263232df9ddd1012ad07be3d2cb145d902127904
Jannatul Ferdous Oni <jannatuloni41@gmail.com>
2025-02-16 00:05:41 +0600

    Create Quick_Sort.cpp

---
+++ Quick_Sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void quickSort(int arr[], int low, int high) {
+    if (low >= high) return;
+
+    int pivot = arr[low];
+    int start = low;
+    int end = high;
+
+    while (start < end) {
+        while (arr[start] <= pivot) {
+            start++;
+        }
+        while (arr[end] > pivot) {
+            end--;
+        }
+        if (start < end) {
+            swap(arr[start], arr[end]);
+        }
+    }
+
+    swap(arr[low], arr[end]);
+
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of array:";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array:";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array:";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
